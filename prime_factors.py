8852dbe836f13c6840b1cfbc1e811abd952edcb5
sanghyung.kim <sh03.kim@samsung.com>
2024-07-10 08:54:08 +0900

    [feature] 1

--- prime_factors.py
+++ prime_factors.py
@@ -1,2 +1,4 @@
 class PrimeFactors:
-    pass
+    def of(self, num):
+        factors = []
+        return factors
--- test_prime_factors.py
+++ test_prime_factors.py
@@ -1,5 +1,9 @@
 from unittest import TestCase
 
+from prime_factors import PrimeFactors
+
 
 class TestPrimeFactors(TestCase):
-    pass
+    def test_prime_factor_of_1(self):
+        prime_factor = PrimeFactors()
+        self.assertEqual([], prime_factor.of(1))
