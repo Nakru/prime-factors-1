8c14619ec6fa95f764db4d823ca8554ae184a2a9
sanghyung.kim <sh03.kim@samsung.com>
2024-07-10 09:33:38 +0900

    [feature] 7

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -25,3 +25,7 @@ class TestPrimeFactors(TestCase):
     def test_prime_factor_of_9(self):
         self.assertEqual([3, 3], self.prime_factors.of(9))
 
+    def test_prime_factor_of_12(self):
+        self.assertEqual([2, 2, 3], self.prime_factors.of(12))
+
+
